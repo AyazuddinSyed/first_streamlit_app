74cf328f7583a3912d2f203c59092afef71ebc36
Ayazuddin Syed <105313101+AyazuddinSyed@users.noreply.github.com>
2022-05-10 22:32:46 +0400

    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 Streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
