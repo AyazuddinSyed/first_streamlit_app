bf447daad04bf2a5621555a77190a61a9285eacf
Ayazuddin Syed <105313101+AyazuddinSyed@users.noreply.github.com>
2022-05-16 07:52:04 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ fruits_selected = streamlit.multiselect("Pick sone fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_list)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
